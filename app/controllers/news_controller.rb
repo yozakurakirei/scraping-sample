5ba303c7ae2aabaac5bd5701fdeada768d4b45e3
yozakura <matchnokaori@gmail.com>
2021-08-09 13:08:40 +0900

    add newsapi

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,11 +2,10 @@ class NewsController < ApplicationController
   require 'open-uri'
 
   def index
-    # api = Rails.application.credentials.news_api[:e0ce889ca64b4880be329380db0dadcf]
-    uri = "https://newsapi.org/v2/everything?q=%E3%82%AF%E3%83%AA%E3%82%B9%E3%83%9E%E3%82%B9&sortBy=popularity&apiKey=e0ce889ca64b4880be329380db0dadcf"
+    uri = "https://newsapi.org/v2/everything?q=%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%0D%0A&sortBy=popularity&pageSize=100&apiKey=e0ce889ca64b4880be329380db0dadcf"
     article = URI.open(uri).read
     @articles = JSON.parse(article)
-    puts @articles
+    # p @articles
   end
 end
 
